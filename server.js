7355f9af963389b4ac6550338f103ccaafc1b810
TomasJLN <tomas.lago@mundo-r.com>
2022-03-19 11:52:18 +0100

    cors fail?

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const bookingExists = require('./middlewares/bookingExists');
 const canVote = require('./middlewares/canVote');
 const userCheckMail = require('./middlewares/userCheckMail');
 //
-app.use(cors());
+app.use(cors({ origin: '*' }));
 
 /** User controllers **/
 const {
