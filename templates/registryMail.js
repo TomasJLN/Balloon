de7cad61916f91466cf7a0ecd815e328fca4ddea
TomasJLN <tomas.lago@mundo-r.com>
2022-03-21 16:08:30 +0100

    test

--- templates/registryMail.js
+++ templates/registryMail.js
@@ -42,9 +42,7 @@ const registryMail = (registryCode) => {
         <h3>Activa tu cuenta de usuario en Balloon</h3>
         <p>Para completar el registro</p>
         <p>pulsa en el globo</p>
-        <a href="${PUBLIC_HOST}/user/validate/${registryCode}" target="_blank" rel="noreferrer"
-            >
-        <a href="http://localhost:3000/account?register=ok" target="_blank" rel="noreferrer"><figure class="hover-rotate">
+        <a href="${PUBLIC_HOST}/user/validate/${registryCode}" target="http://localhost:3000/account?register=ok" rel="noreferrer"><figure class="hover-rotate">
                 <img
                     src="https://imagizer.imageshack.com/img922/9099/Sj6jBW.png"
                     alt="🎈 Logo Balloon"
